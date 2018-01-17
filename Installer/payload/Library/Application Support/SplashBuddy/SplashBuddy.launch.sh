e7ee27debf7087b63e7e990e6d25c9977b2f9996
doggles <8885886+doggles@users.noreply.github.com>
2018-01-17 12:15:13 -0500

    Removed internal references

--- Installer/payload/Library/Application Support/SplashBuddy/SplashBuddy.launch.sh
+++ Installer/payload/Library/Application Support/SplashBuddy/SplashBuddy.launch.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 loggedInUserID=$(id -u $loggedInUser)
 
 APP_PATH="/Library/Application Support/SplashBuddy/SplashBuddy.app"
 DONE_FILE="/var/db/.SplashBuddyDone"
-ICON_FILE="/Library/Application Support/SplashBuddy/jeff-icon.png"
-CD_PATH="/Library/Application Support/SplashBuddy/cocoaDialog.app/Contents/MacOS/cocoaDialog"
 
-function getAssetTag() {
-	assetTag=$(launchctl asuser $loggedInUserID osascript -e 'set jeffIcon to POSIX file "'"$ICON_FILE"'"' -e 'display dialog "Enter Asset Tag #:" default answer "" giving up after 86400 with icon jeffIcon with text buttons {"OK"} default button 1' -e 'return text returned of result')
-	until [[ $assetTag =~ ^[aA][pP][cC][0-9a-dA-D]([0-9]){3}$ ]]; do
-	assetTag=$(launchctl asuser $loggedInUserID osascript -e 'set jeffIcon to POSIX file "'"$ICON_FILE"'"' -e 'display dialog "Invalid Asset Tag. Try again:" default answer "" giving up after 86400 with icon jeffIcon with text buttons {"OK"} default button 1' -e 'return text returned of result')
+function getAssetNum() {
+	assetNum=$(launchctl asuser $loggedInUserID osascript -e 'display dialog "Enter Asset Tag #:" default answer "" giving up after 86400 with text buttons {"OK"} default button 1' -e 'return text returned of result')
+	until $assetNum; do
+	assetNum=$(launchctl asuser $loggedInUserID osascript -e 'display dialog "Invalid Asset Tag. Try again:" default answer "" giving up after 86400 with text buttons {"OK"} default button 1' -e 'return text returned of result')
 	done
-	assetTagVerify=$(launchctl asuser $loggedInUserID osascript -e 'set jeffIcon to POSIX file "'"$ICON_FILE"'"' -e 'display dialog "Confirm Asset Tag #:" default answer "" giving up after 86400 with icon jeffIcon with text buttons {"Confirm"} default button 1' -e 'return text returned of result')
+	confirm=$(launchctl asuser $loggedInUserID osascript -e 'display dialog "Confirm Asset Tag #:" default answer "" giving up after 86400 with text buttons {"Confirm"} default button 1' -e 'return text returned of result')
 }
 
 function appInstalled {
@@ -36,37 +34,24 @@ if appNotRunning \
 
 	sleep 3
 
-	getAssetTag
-	until [[ $assetTagVerify = "$assetTag" ]]
-	do getAssetTag
+### Rename machine to provided asset number
+	getAssetNum
+	until [ "$confirm" = "$assetTag" ]
+	do getAssetNum
 	done
 
-	assetTag=$(tr '[:lower:]' '[:upper:]' <<< "$assetTag")
+	scutil --set ComputerName "$assetNum"
+	scutil --set LocalHostName "$assetNum"
+	scutil --set LocalHostName ''
 
-	scutil --set ComputerName "$assetTag"
-	scutil --set LocalHostName "$assetTag"
+### Bind using jamf policy
+	( /usr/local/bin/jamf policy -trigger bind ) & PID=$!
 
-	userDomain=$("$CD_PATH" standard-dropdown \
-  	--title "Jefferson IS&T" \
-		--text " Select the user's assigned domain:
-    " \
-		--items "University" "Hospital" \
-		--no-newline \
-		--no-cancel \
-		--float \
-		--string-output \
-		--icon-file "$ICON_FILE"
-	)
-
-	if [[ $userDomain =~ Hospital ]]
-	then ( /usr/local/bin/jamf policy -trigger enrollBindTJUH ) & PID=$!
-	elif [[ $userDomain =~ University ]]
-	then ( /usr/local/bin/jamf policy -trigger enrollBindTJU ) & PID=$!
-	fi
-
+### Launch SplashBuddy.app as console user
   launchctl asuser $loggedInUserID open -a "$APP_PATH"
 
-	while kill -0 $PID &>/dev/null
+### Wait until domain binding policy completes
+	while kill -0 $PID &> /dev/null
 	do sleep 0.1
 	done
 
@@ -74,7 +59,7 @@ if appNotRunning \
 
 elif [ -f "$DONE_FILE" ]; then
 
-	launchctl remove -F io.fti.SplashBuddy.launch
+	launchctl bootout system/io.fti.SplashBuddy.launch
 	rm -rf /Library/Application\ Support/SplashBuddy
 	rm -f /Library/LaunchDaemons/io.fti.SplashBuddy.launch.plist
 	rm -f /Library/Preferences/io.fti.SplashBuddy.plist
